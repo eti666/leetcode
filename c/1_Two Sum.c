70891278262c78d68208c4ed6ee20fae05517adc
WenqiangXie <wenqiangxie@live.com>
2019-01-21 20:43:52 +0800

    Update 1_Two Sum.c
    update code of the 1st problem with c code. and update used time to 44ms in test case.

--- c/1_Two Sum.c
+++ c/1_Two Sum.c
@@ -1,6 +1,25 @@
-#include <stdio.h>
-
-int main()
-{
-    return 0;
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+// this solution in test case used 44 ms.
+int* twoSum(int* nums, int numsSize, int target) {
+    
+    for(int i=0; i<numsSize; i++)
+    {
+        // as we know, we just test the second number(which index j) just
+        // after the first number(which index j).
+        for(int j = i+1; j < numsSize; j++)
+        {
+            if(nums[j] == (target-nums[i])) 
+            {
+                // found. return.
+                int *index = (int *)malloc(2 * sizeof(int));
+                index[0] = i;
+                index[1] = j;
+                return index;
+            }
+        }
+    }
+    // not found, return NULL.
+    return NULL;
 }
